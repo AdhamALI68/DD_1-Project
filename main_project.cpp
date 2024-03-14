b299d9ebd1aa89ddada3d77658223d3674ee153d
omarsaqr12 <omar_saqr@aucegypt.edu>
2024-03-14 19:40:17 +0200

    This file deals with the input, and calls the functions correctly, we will make it more scalable by finding a way other than map of functions, to call the functions, and we will now create a new function that deals with the code to make it clear when we change inputs using the .stim file

--- main_project.cpp
+++ main_project.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-using LogicFunction = function<bool(const vector<bool>&, int&)>;
+using LogicFunction = function<bool(const vector<bool>&, int)>;
 
 // Define basic logic functions with delay parameter
-bool logic_AND(const vector<bool>& inputs, int& delay) {
+bool logic_AND(const vector<bool>& inputs, int delay) {
     bool result = true;
     for (bool input : inputs) {
         result = result && input;
@@ -12,7 +12,7 @@ bool logic_AND(const vector<bool>& inputs, int& delay) {
     return result;
 }
 
-bool logic_OR(const vector<bool>& inputs, int& delay) {
+bool logic_OR(const vector<bool>& inputs, int delay) {
     bool result = false;
     for (bool input : inputs) {
         result = result || input;
@@ -21,13 +21,13 @@ bool logic_OR(const vector<bool>& inputs, int& delay) {
     return result;
 }
 
-bool logic_NAND(const vector<bool>& inputs, int& delay) {
+bool logic_NAND(const vector<bool>& inputs, int delay) {
     bool result = !(logic_AND(inputs, delay));
     // Modify delay if needed
     return result;
 }
 
-bool logic_XOR(const vector<bool>& inputs, int& delay) {
+bool logic_XOR(const vector<bool>& inputs, int delay) {
     bool result = false;
     for (bool input : inputs) {
         result = result ^ input;
@@ -36,7 +36,7 @@ bool logic_XOR(const vector<bool>& inputs, int& delay) {
     return result;
 }
 
-bool logic_NOT(const vector<bool>& inputs, int& delay) {
+bool logic_NOT(const vector<bool>& inputs, int delay) {
     vector<bool> inverted_inputs = inputs; // Copy inputs to avoid modifying original vector
     for (int i = 0; i < inputs.size(); ++i) {
         inverted_inputs[i] = !inverted_inputs[i]; // Invert each input
@@ -47,11 +47,11 @@ bool logic_NOT(const vector<bool>& inputs, int& delay) {
 
 // Create a map to store logic functions
 unordered_map<string, LogicFunction> logic_functions = {
-    {"AND", logic_AND},
-    {"OR", logic_OR},
-    {"NAND", logic_NAND},
-    {"XOR", logic_XOR},
-    {"NOT", logic_NOT}
+        {"AND", logic_AND},
+        {"OR", logic_OR},
+        {"NAND", logic_NAND},
+        {"XOR", logic_XOR},
+        {"NOT", logic_NOT}
 };
 
 
@@ -117,9 +117,48 @@ void parse_cir_file(const string& filename, vector<string>& inputs, vector<vecto
     file.close();
 }
 
+double simulate_circuit(const vector<vector<string>>& components, unordered_map<string, int>& input_map) {
+    int total_delay = 0;
+
+    // Iterate through components
+    for (const auto& component : components) {
+        string gate_name = component[0];
+        string function_name = component[1];
+        string output_wire = component[2];
+
+        // Get delay and logic function for the component
+        int delay = component_library[function_name].delay_ps;
+        LogicFunction logic_function = logic_functions[function_name];
+
+        // Extract input wires
+        vector<string> input_wires;
+        for (size_t i = 3; i < component.size(); ++i) {
+            if (component[i] != ",") {
+                input_wires.push_back(component[i]);
+            }
+        }
+
+        // Get input values from input_map
+        vector<bool> inputs;
+        for (const auto& input_wire : input_wires) {
+            inputs.push_back(input_map[input_wire]);
+        }
+
+        // Perform logic operation and update output wire value in input_map
+        bool result = logic_function(inputs, delay);
+        input_map[output_wire] = result;
+
+        // Accumulate delay
+        total_delay += delay;
+    }
+
+    return total_delay;
+}
+
+
 int main() {
     loadLibrary("lib.txt");
-    
+
     // Example usage
     for (const auto& x : component_library) {
         cout << "Component: " << x.first <<" "<<x.second.num_inputs<<" "<<x.second.delay_ps<<"\n"  ;
@@ -127,11 +166,12 @@ int main() {
     vector<string> inputs;
     vector<vector<string>> components;
     parse_cir_file("cir.txt", inputs, components);
+    
 
     // Store inputs in a map and initialize to zero
     unordered_map<string, int> input_map;
     for (const string& input : inputs) {
-        input_map[input] = 0;
+        input_map[input] = 1;
     }
 
     // Store component functions and number of parameters
@@ -144,16 +184,61 @@ int main() {
 
     // Example usage:
     // Printing the input map
-    cout << "Inputs:" << endl;
-    for (const auto& x: input_map) {
-        cout << x.first << ": " << x.second << endl;
+    unordered_map<string, int> operation;
+    operation=component_functions;
+    
+  
+
+    for (int i = 0; i < components.size(); i++)
+    {
+        string output="";
+        string gatename="";
+        vector<string>in;
+        vector<bool>inp;
+        for (int j = 0; j <components[i].size(); j++)
+        {
+            if(operation[components[i][j]]!=0){
+                if(gatename==""){gatename=components[i][j]; gatename.pop_back(); }
+               output=components[i][j+1];          j++; continue; }
+
+               if(output!=""){
+                if(components[i][j][components[i][j].length()-1]==','){components[i][j].erase(components[i][j].length()-1);}
+                in.push_back(components[i][j]);
+                if(input_map.find(components[i][j])==input_map.end()){
+                    input_map[components[i][j]]=0;}
+                    
+                }
+
+                    }
+                    for (int k = 0; k < in.size(); k++)
+                    {
+                       inp.push_back(input_map[in[k]]);
+                    }
+                    string z=gatename+',';
+                    output.pop_back();
+                    if(gatename=="NOT"){ input_map[output]=logic_NOT(inp,component_library[z].delay_ps);}
+                    gatename.pop_back();
+                    if(gatename=="AND"){ input_map[output]=logic_AND(inp,component_library[z].delay_ps);}
+                    if(gatename=="OR"){ input_map[output]=logic_OR(inp,component_library[z].delay_ps);}
+                    if(gatename=="XOR"){ input_map[output]=logic_XOR(inp,component_library[z].delay_ps);}
+
+                    in.clear();
+        cout<<"\n";
     }
+    
+        cout << "Inputs:" << endl;
 
     // Printing the component functions and number of parameters
-    cout << "Component Functions:" << endl;
-    for (const auto&x : component_functions) {
-        cout << x.first << ": " << x.second << " parameters" << endl;
+      for (const auto& x: input_map) {
+        cout << x.first << ": " << x.second << endl;
     }
+    // for (const auto&x : component_functions) {
+    //     cout << x.first << ": " << x.second << " parameters" << endl;
+    // }
+
+
+
+
 
     return 0;
 }
