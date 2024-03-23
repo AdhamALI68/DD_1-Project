5a5dc586728ce4cb5ea9972249e574c3bdd94316
omarsaqr12 <125351767+omarsaqr12@users.noreply.github.com>
2024-03-23 16:27:17 +0200

    Update Custom_gates.cpp
    Now it can handle inputs that are not a single letter for example instead of a&b now it can support in0 & in1

--- Custom_gates.cpp
+++ Custom_gates.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class LogicalExpressionEvaluator {
 private:
     bool isOperator(char c) {
-        return (c == '&' || c == '|' || c == '~' );
+        return (c == '&' || c == '|' || c == '~' || c == '(' || c == ')');
     }
 
     int precedence(char c) {
@@ -19,6 +19,9 @@ private:
                 return 2; // Adjusted for clarity, though not necessary
             case '~':
                 return 3; // Highest precedence for unary operator
+            case '(':
+            case ')':
+                return 0; // Parentheses have highest precedence
         }
         return -1;
     }
@@ -43,54 +46,66 @@ private:
         return false; // Should never reach here
     }
 
+    // Function to extract the next operand (variable) from the expression
+    string extractOperand(string& expression, int& pos) {
+        string operand = "";
+        while (pos < expression.length() && !isOperator(expression[pos])) {
+            operand += expression[pos];
+            pos++;
+        }
+        return operand;
+    }
+
 public:
-    bool evaluateInfixExpression(string expression, unordered_map<char, bool>& variables) {
+    bool evaluateInfixExpression(string expression, unordered_map<string, bool>& variables) {
         stack<bool> operands;
         stack<char> operators;
         int i = 0;
 
         while (i < expression.length()) {
             char c = expression[i];
-            if (isalpha(c)) {
-                operands.push(variables[c]);
-            } else if (c == '(') {
+            if (!isOperator(c)) {
+                string operand = extractOperand(expression, i);
+                cout<<"operand "<<operand<<"\n";
+                if (variables.find(operand) != variables.end()) {
+                    operands.push(variables[operand]);
+                } else {
+                    throw "Undefined variable: " + operand;
+                }
+                continue; // Skip to the next iteration
+            }
+            if (c == '(') {
                 operators.push(c);
             } else if (c == ')') {
                 while (operators.top() != '(') {
                     if (operators.top() == '~') {
                         bool a = operands.top();
                         operands.pop();
-                        char op = operators.top();
-                        operators.pop();
-                        operands.push(evaluate(a, op));
+                        operands.push(evaluate(a, operators.top()));
                     } else {
                         bool b = operands.top();
                         operands.pop();
                         bool a = operands.top();
                         operands.pop();
-                        char op = operators.top();
-                        operators.pop();
-                        operands.push(evaluate(a, b, op));
+                        operands.push(evaluate(a, b, operators.top()));
                     }
+                    operators.pop();
                 }
-                operators.pop();
+                operators.pop(); // Pop '('
             } else if (isOperator(c)) {
                 while (!operators.empty() && precedence(c) <= precedence(operators.top())) {
                     if (operators.top() == '~') {
                         bool a = operands.top();
                         operands.pop();
-                        char op = operators.top();
-                        operators.pop();
-                        operands.push(evaluate(a, op));
+                        operands.push(evaluate(a, operators.top()));
                     } else {
                         bool b = operands.top();
                         operands.pop();
                         bool a = operands.top();
                         operands.pop();
-                        char op = operators.top();
-                        operators.pop();
-                        operands.push(evaluate(a, b, op));
+                        operands.push(evaluate(a, b, operators.top()));
                     }
+                    operators.pop();
                 }
                 operators.push(c);
             }
@@ -101,18 +116,15 @@ public:
             if (operators.top() == '~') {
                 bool a = operands.top();
                 operands.pop();
-                char op = operators.top();
-                operators.pop();
-                operands.push(evaluate(a, op));
+                operands.push(evaluate(a, operators.top()));
             } else {
                 bool b = operands.top();
                 operands.pop();
                 bool a = operands.top();
                 operands.pop();
-                char op = operators.top();
-                operators.pop();
-                operands.push(evaluate(a, b, op));
+                operands.push(evaluate(a, b, operators.top()));
             }
+            operators.pop();
         }
 
         return operands.top();
@@ -121,13 +133,13 @@ public:
 
 int main() {
     LogicalExpressionEvaluator evaluator;
-    unordered_map<char, bool> variables = {
-        {'a', true},
-        {'b', false},
-        {'c', true},
-        {'d',true}
+    unordered_map<string, bool> variables = {
+        {"in0", true},
+        {"in1", false},
+        {"in2", true},
+        {"in3", true}
     };
-    string expression = "(~d|(a&b))&(a&c)";
+    string expression = "(in3|(in0&in1))&(in0&in2)";
     try {
         bool result = evaluator.evaluateInfixExpression(expression, variables);
         cout << "Result: " << (result ? "true" : "false") << endl;
