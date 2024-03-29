dc4e7133f06ee3e48b66c9765be0688403fcb427
BeTechBo <123619092+BeTechBo@users.noreply.github.com>
2024-03-29 07:44:44 +0200

    Edited axes

--- Graphing/SG.py
+++ Graphing/SG.py
@@ -29,15 +29,16 @@ def Graph_one_line(ax, t, list_of_line, offset):
     ax.step(t, lst_of_binary, rndm.choice(colors), label=list_of_line[2], linewidth=3, where='post')
     ax.legend()
 
-def graph_all_outs(t, two_d_list):
+def graph_all_outs(t, two_d_list, offset):
     num_lines = len(two_d_list)
     fig, axes = plt.subplots(num_lines, 1, figsize=(10, 5*num_lines))
     
     for i, ax in enumerate(axes):
         Graph_one_line(ax, t, two_d_list[i], i * 3)
         axes[i].locator_params(axis = 'y', nbins= 2)
+        ax.set_ylim([-0.1,1.1])
 
     fig.text(0.5, 0.01, 'Time (ps)', ha='center', va='center')
     fig.text(0.06, 0.5, 'Output', ha='center', va='center', rotation='vertical')
-    plt.tight_layout(pad = 5.0)
+    plt.tight_layout(pad = 5/4 * offset)
     plt.show()
--- Graphing/simulation_graphing.py
+++ Graphing/simulation_graphing.py
@@ -2,7 +2,7 @@ import nltk as nl
 import SG
 import numpy as np
 
-file = open("D:\Spring 2024\Digital Design\Project\Final project\Graphing\output.sim" , "r")
+file = open("D:\Spring 2024\Digital Design\Project\The Final Project\DD_1-Project\Graphing\output.sim" , "r")
 files_tokens = file.readlines()
 files_tokens_of_tokens = []
 
@@ -10,4 +10,4 @@ for i in range(len(files_tokens)):
     files_tokens_of_tokens.append(nl.word_tokenize(files_tokens[i]))
 
 t = np.arange(1500)
-SG.graph_all_outs(t,files_tokens_of_tokens)
+SG.graph_all_outs(t,files_tokens_of_tokens, len(files_tokens))
