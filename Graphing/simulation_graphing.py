066ea9faaa784359f036260820dfadd125799dbb
BeTechBo <123619092+BeTechBo@users.noreply.github.com>
2024-03-30 12:55:15 +0200

    Update simulation_graphing.py

--- Graphing/simulation_graphing.py
+++ Graphing/simulation_graphing.py
@@ -1,13 +1,19 @@
 import nltk as nl
 import SG
 import numpy as np
+import math
 
-file = open("D:\Spring 2024\Digital Design\Project\The Final Project\DD_1-Project\Graphing\output.sim" , "r")
+file = open("D:\Spring 2024\Digital Design\Final version of the code\DD_1-Project\Graphing\out.sim" , "r")
 files_tokens = file.readlines()
 files_tokens_of_tokens = []
 
 for i in range(len(files_tokens)):
     files_tokens_of_tokens.append(nl.word_tokenize(files_tokens[i]))
 
-t = np.arange(1500)
-SG.graph_all_outs(t,files_tokens_of_tokens, len(files_tokens))
+z = 0
+for i in range(len(files_tokens_of_tokens)):
+    if int(files_tokens_of_tokens[i][0]) > int(z):
+        z = files_tokens_of_tokens[i][0]
+
+t = np.arange(math.floor(int(z)*1.5))
+SG.graph_all_outs(t,files_tokens_of_tokens, len(files_tokens) , int(int(z)*1.5) , 5)
